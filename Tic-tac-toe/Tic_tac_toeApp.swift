ccc57f030b297daa4e9d4a417a415f0f22f96d5a
Vasily Shuklin <b88bb@yandex.ru>
2024-05-29 19:41:28 +0500

    Refactor Tic_tac_toeApp View with viewModel

--- Tic-tac-toe/Tic_tac_toeApp.swift
+++ Tic-tac-toe/Tic_tac_toeApp.swift
@@ -2,9 +2,13 @@ import SwiftUI
 
 @main
 struct Tic_tac_toeApp: App {
+    
+    @StateObject var gameViewModel: GameViewModel = GameViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(gameViewModel)
         }
     }
 }
