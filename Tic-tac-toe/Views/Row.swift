f43cfd682b5047accf69a25a928eb4c3cef0822b
Vasily Shuklin <b88bb@yandex.ru>
2024-05-30 19:48:52 +0500

    Update Row View with @Binding vars

--- Tic-tac-toe/Views/Row.swift
+++ Tic-tac-toe/Views/Row.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct Row: View {
+    @Binding var newGame: Bool
+    @Binding var isNextNought: Bool
+    
+    init(newGame: Binding<Bool>, isNextNought: Binding<Bool>) {
+        _newGame = newGame
+        _isNextNought = isNextNought
+    }
     
     var body: some View {
         HStack {
-            Cell()
-            Cell()
-            Cell()
+            Cell(newGame: $newGame, isNextNought: $isNextNought)
+            Cell(newGame: $newGame, isNextNought: $isNextNought)
+            Cell(newGame: $newGame, isNextNought: $isNextNought)
             }
         .aspectRatio(contentMode: .fit)
     }
@@ -15,6 +22,6 @@ struct Row: View {
 
 struct Row_Previews: PreviewProvider {
     static var previews: some View {
-        Row()
+        Row(newGame: .constant(false), isNextNought: .constant(false))
     }
 }
