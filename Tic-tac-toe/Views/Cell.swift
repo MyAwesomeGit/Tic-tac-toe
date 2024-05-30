fdd6608038f0a1eaed7b6028fd1d18e6532e7116
Vasily Shuklin <b88bb@yandex.ru>
2024-05-30 19:47:49 +0500

    Update Cell View with @Binding vars

--- Tic-tac-toe/Views/Cell.swift
+++ Tic-tac-toe/Views/Cell.swift
@@ -3,15 +3,18 @@ import SwiftUI
 struct Cell: View {
     
     @StateObject var viewModel: CellViewModel
-    @EnvironmentObject var gameViewModel: GameViewModel
+    @Binding var newGame: Bool
+    @Binding var isNextNought: Bool
     
-    init() {
+    init(newGame: Binding<Bool>, isNextNought: Binding<Bool>) {
         _viewModel = StateObject(wrappedValue: CellViewModel())
+        _newGame = newGame
+        _isNextNought = isNextNought
     }
     
     @ViewBuilder
     private var content: some View {
-        if gameViewModel.newGame == true {
+        if newGame == true {
             Color.clear
         } else {
             switch viewModel.cellType {
@@ -30,26 +33,32 @@ struct Cell: View {
             .padding(20)
             .contentShape(Rectangle())
             .onTapGesture {
-                if gameViewModel.newGame == true {
-                    viewModel.cellType = .hidden
+                if newGame == true {
+                    newGame = false
                 }
                 if viewModel.cellType == .nought || viewModel.cellType == .cross {
                     return
                 } else {
-                    viewModel.cellType = gameViewModel.isNextNought ? .nought : .cross
-                    gameViewModel.isNextNought.toggle()
+                    viewModel.cellType = isNextNought ? .nought : .cross
+                    isNextNought.toggle()
                 }
             }
             .aspectRatio(contentMode: .fit)
             .background(Color.gray.opacity(0.9))
             .background(Color.green.opacity(0.5))
+            .onChange(of: $newGame.wrappedValue) { oldValue, newValue in
+                if newValue == true {
+                    viewModel.cellType = .hidden
+                    
+                }
+            }
     }
 }
 
 
 struct Cell_Previews: PreviewProvider {
     static var previews: some View {
-        Cell()
+        Cell(newGame: .constant(false), isNextNought: .constant(false))
             .aspectRatio(contentMode:.fit)
     }
 }
